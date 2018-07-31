30c1b0e83d439442a5b292d5a9e417c6187421a5
Steve Purzhash <devolper7@gmail.com>
2018-07-31 13:36:46 +0300

    Starting App

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    
+    mongoURI: 
+    'mongodb://Steve:qwerty12@ds161411.mlab.com:61411/devconnectorstev'
+};
---
+++ routes/api/posts.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+//@route GET api/posts/test
+//@desc Tests post route
+//@access Public
+
+router.get("/test", (req, res) =>
+  res.json({
+    msg: "Post Works"
+  })
+);
+
+module.exports = router;
